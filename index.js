087b4296d33f546c8bd0c327254295a35a5e37fb
escray <zescray@gmail.com>
2016-02-01 09:00:40 +0800

    Implement Error Handling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = myexpress;
+
 var url = require("url");
 var http = require("http");
 
@@ -7,21 +8,67 @@ function myexpress() {
   
 
   var app = function(req, res) {
+    function resEnd(code) {
+      res.statusCode = code;
+      var msg;
+      if (code == 500) {
+        msg = "500 - Internal Error";
+      } else if (code == 404) {
+        msg = "404 - Not Found";
+      }
+      res.end(msg);
+    }
+
+
     if (url.parse(req.url).path === '/foo') {
-        res.statusCode = 404;
-        res.end();  
+        // res.statusCode = 404;
+        // res.end("404 - Not Found");  
+        resEnd(404);
     };
 
-    next = function() {
-      if (app.stack[i]) {
-        return app.stack[i++](req, res, next);
-      } else {
-        res.statusCode = 404;
-        res.end();
+    next = function(err) {
+      if (err) {
+        // res.statusCode = 500;
+        // res.end();
+        // if (i == app.stack.length) {
+        //   resEnd(500);
+        //   return;
+        // };
+        // resEnd(500);
+        while (app.stack[i] && app.stack[i].length != 4) {
+            i++
+        }
+        if (i == app.stack.length) {
+              resEnd(500);
+              return;
+        };
+        if (app.stack[i])
+          return app.stack[i++](err, req, res, next);
+        else {
+          resEnd(404);
+        }        
       }
+      else {
+        while (app.stack[i] && app.stack[i].length != 3) {
+          i++;
+        }
+        if (app.stack[i]) {
+          try {
+            return app.stack[i++](req, res, next);          
+          }
+          catch (err) {
+            next(err);
+          }
+        } else {
+          resEnd(404);
+        }
+
+        
+      }      
     }
 
     next();
+
   }
 
   app.listen = function() {
@@ -33,13 +80,9 @@ function myexpress() {
   var i = 0;
 
   app.use = function(middleware) {
-    app.stack.push(middleware);
+    app.stack.push(middleware);  
     return app;    
   }
-
-  
-
-
-
   return app; 
 }
+
--- test/app_spec.js
+++ test/app_spec.js
@@ -103,5 +103,63 @@ describe("Building the middlewares stack", function() {
     });
   });
 
-  
+  describe("Implement error handling", function() {
+      var app;
+
+      beforeEach(function() {
+        app = express();
+      });
+
+      it("should return 500 for unhandled error", function(done) {
+        var m1 = function(req, res, next) {
+          next(new Error("boom!"));
+        }
+        app.use(m1);
+        request(app).get('/').expect(500).end(done);
+      });
+
+      it("should return 500 for uncaught error", function(done) {
+        var m1 = function(req, res, next) {
+          throw new Error("boom!");
+        };
+        app.use(m1);
+        request(app).get('/').expect(500).end(done);
+      });
+
+      debugger;
+      it("should ignore error handlers when 'next' is called without an error", function(done) {
+        var m1 = function(req, res, next) {
+          next();
+        }
+        var e1 = function(err, req, res, next) {
+          // timeout
+        }
+        var m2 = function(req, res, next) {
+          res.end("m2");
+        }
+        app.use(m1);
+        app.use(e1);
+        app.use(m2);
+
+        request(app).get('/').expect("m2").end(done);
+
+      });
+
+      it("should skip normal middlewares if 'next' is called with an error", function(done) {
+        var m1 = function(req, res, next) {
+          next(new Error("boom!"));
+        }
+        var m2 = function(req, res, next) {
+          // timeout
+        }
+        var e1 = function(err, req, res, next) {
+          res.end("e1")
+        }
+        app.use(m1);
+        app.use(m2);
+        app.use(e1);
+
+        request(app).get('/').expect("e1").end(done);
+      });
+  });  
 });
