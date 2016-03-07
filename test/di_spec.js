5a80ce813d82bc01f4fedb88d46b3c4f41939a80
escray <zescray@gmail.com>
2016-03-07 22:08:52 +0800

    lost some change

--- test/di_spec.js
+++ test/di_spec.js
@@ -191,7 +191,7 @@ describe("Implement Injector Invokation: ", function() {
     var req = 1, res = 2;
     function next(err) {
       expect(err).to.be.instanceof(Error);
-      expect(err.message).to.equal("Factory not defined: unknow_dep");
+      expect(err.message).to.equal("Factory not defined: unknown_dep");
     }
 
     function handler(unknown_dep) {};
